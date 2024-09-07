679af4cd9135b0c2d47ca75c7b03b001dda4ca53
Rupali Kesharwani <kesrupali13@gmail.com>
2024-09-07 21:04:53 +0530

    Created movie model

---
+++ MovieApp/MovieApp/Models/Movie.swift
@@ -0,0 +1,46 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Rupali Kesharwani on 07/09/24.
+//
+
+import Foundation
+
+struct Movie: Codable {
+	let adult: Bool?
+	let backdropPath: String?
+	let genreIds: [Int]?
+	let id: Int?
+	let originalLanguage: String?
+	let originalTitle: String?
+	let overview: String?
+	let popularity: Double?
+	let posterPath: String?
+	let releaseDate: String?
+	let title: String?
+	let video: Bool?
+	let voteAverage: Double?
+	let voteCount: Int?
+	
+	var posterUrl: String {
+		return "https://image.tmdb.org/t/p/w500/\(posterPath ?? "")"
+	}
+	
+	enum CodingKeys: String, CodingKey {
+		case adult
+		case backdropPath = "backdrop_path"
+		case genreIds = "genre_ids"
+		case id
+		case originalLanguage = "original_language"
+		case originalTitle = "original_title"
+		case overview
+		case popularity
+		case posterPath = "poster_path"
+		case releaseDate = "release_date"
+		case title
+		case video
+		case voteAverage = "vote_average"
+		case voteCount = "vote_count"
+	}
+}
--- MovieApp/MovieApp/SplashViewController.swift
+++ MovieApp/MovieApp/SplashViewController.swift
@@ -26,3 +26,6 @@ class SplashViewController: UIViewController {
 	}
 }
 
+struct codeF : Codable {
+
+}
